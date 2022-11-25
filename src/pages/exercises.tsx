c431a3a6536b6095140f2f9566773aee8d480a02
Nicolas Del Piano <ndel314@gmail.com>
2022-11-25 17:02:00 -0300

    Remove unused form

--- src/pages/exercises.tsx
+++ src/pages/exercises.tsx
@@ -105,58 +105,6 @@ const ExercisesPage: NextPage = () => {
       </h2>
       <div className="pt-6">
         <Form />
-        <form
-          method="get"
-          action="/exercises"
-          onSubmit={(e) => {
-            e.preventDefault();
-            // console.log(e.target.exercise_name.value);
-          }}
-        >
-          <div className="mb-6 grid gap-6 md:grid-cols-2">
-            <div>
-              <label
-                htmlFor="exercise_name"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Name
-              </label>
-              <input
-                type="text"
-                id="exercise_name"
-                className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
-                placeholder="Bench press, dips, squats..."
-                required
-              />
-            </div>
-
-            {/* <div>
-              <label
-                htmlFor="exercise_name"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Name
-              </label>
-              <MyMenu />
-            </div> */}
-            <div>
-              <label
-                htmlFor="muscle"
-                className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-              >
-                Muscle
-              </label>
-              {/* <Example /> */}
-            </div>
-          </div>
-
-          <button
-            className="rounder-lg block w-full border border-gray-300"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
       </div>
     </div>
   );
